75ce22c231774014650d47c531e16ee2324a0c69
TirthakBM <tirthakbm123@gmail.com>
2023-07-25 10:50:05 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <section className="flexStart flex-col paddings">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load More</h1>
+
+      {/* <p className="no-result-text text-center">No projects found, go create some first.</p> */}
+    </section>
+  );
+};
+
+export default Home;
